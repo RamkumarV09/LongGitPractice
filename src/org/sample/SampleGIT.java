eb3543151075c3b9ebfb3e1a1b0ab3de046f2ca7
RamkumarV09 <ramkumarv0986@gmail.com>
2025-08-04 14:42:18 +0530

    Ram Code

---
+++ src/org/sample/SampleGIT.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+public class SampleGIT {
+	
+	public void tc1() {
+
+		System.out.println("TC1");
+	}
+	
+	public void tc2() {
+
+		System.out.println("TC2");
+	}
+	
+	public void tc3() {
+		
+		System.out.println("TC3");
+
+	}
+	
+	
+
+}
