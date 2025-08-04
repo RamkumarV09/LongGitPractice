99a3d0247cc7b60a9aee86ab407793f9b51e7b78
RamkumarV09 <ramkumarv0986@gmail.com>
2025-08-04 15:18:41 +0530

    Green Code

--- src/org/sample/SampleGIT.java
+++ src/org/sample/SampleGIT.java
@@ -1,23 +1,32 @@
 package org.sample;
 
 public class SampleGIT {
-	
+
 	public void tc1() {
 
 		System.out.println("TC1");
 	}
-	
+
 	public void tc2() {
 
 		System.out.println("TC2");
 	}
-	
+
 	public void tc3() {
-		
+
 		System.out.println("TC3");
 
 	}
-	
-	
 
+	public void tc6() {
+
+		System.out.println("TC6");
+
+	}
+
+	public void tc7() {
+
+		System.out.println("TC7");
+
+	}
 }
